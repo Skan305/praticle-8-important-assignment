472cfddb608f3bb0eb25f0fd04c2d6bebbd095af
skhan305 <usajjad028@gmail.com>
2024-07-19 11:21:16 +0500

    add the file

---
+++ script.js
@@ -0,0 +1,184 @@
+// generate random number between 1 and 100;
+// let a=Math.round(Math.random()*100);
+// console.log(a)
+
+
+// it is generate random number two input user
+// let a=prompt("enter a number")
+// let b=prompt("enter a number")
+// let c=Math.round(Math.random(a,b)*10);
+// alert(c)
+
+
+// let a=+prompt("select one 0 or 1")
+// let b=Math.round(Math.random(a)*1);
+// if(a<=0)
+// {
+//     alert(" head" )
+// }
+// else if (a<=1){
+// alert("talis")
+// }
+// else{
+//     alert("out of range")
+// }
+    
+
+
+
+
+
+// converting a string into integer and decimal
+
+// let a="2351";
+// let b=parseInt(a)
+// console.log(typeof b)
+// console.log(b)
+
+// let a="235.1";
+// let b=parseFloat(a)
+// console.log( b)
+
+
+// let a=prompt("enter value")
+// let b=Number(a);
+// alert("convert your value " + b)
+
+
+// Getting the current date and time
+
+//  let a= new Date()
+//  console.log(a)
+
+
+// let a=new Date()
+// let b=a.getFullYear()
+// console.log(b)
+
+
+// let a=new Date()
+// let b=a.getMonth()
+// console.log(b)
+
+
+// random Paswoord generater 
+// let a="ABCDEFGHIJKLMNOPQRSTUVWXYZ12345678910";
+// let Paswoord="";
+// for(let i=0;i<12;i++){
+//     let b=Math.floor(Math.random()*a.length);
+// Paswoord += a[b]
+//     console.log(Paswoord)
+// }
+
+
+
+// convert the strings to number ,number to strings
+
+// let a="234";
+// let b=Number(a);
+// console.log(typeof b)
+// console.log(b)
+
+
+// let a=234;
+// let b=a.toString();
+// console.log(typeof b)
+// console.log(b)
+
+
+// alert("choice one ")
+// let a=prompt(" strings to number")
+// let b=parseInt(a)
+// alert(  " It is convert from strings to number  type is ..." +  b + " " + typeof b)
+// let c=prompt("convert Number to strings")
+// let d=toString(c)
+// alert(  " It is convert from number to strings type is ..."  +  b + " " + typeof c)
+// if (a==String){
+//     let b=parseInt(a);
+//     alert("it is convert to number " + b)
+// }
+// else if (a==Number){
+//     let c=a.toString();
+//     alert(c)
+// }
+// else{
+//     alert("not found")
+// }
+
+   
+
+
+// Controoing the length of decimals using tofixed method
+
+// let a= 234.5643;
+// let b=a.toFixed(2)
+// console.log(b)
+
+
+// let a= 234.5643532000;
+// let b=a.toFixed(4)
+// console.log(b)
+
+// var a=+prompt("enter decimal value");
+// var b=a.toFixed(2);
+// alert(b)
+
+
+
+// Extracting parts of the date and time
+
+// let a=new Date;
+// let b= a.getFullYear();
+// console.log(b)
+
+
+// let a=new Date;
+// let b= a.getMonth();
+// console.log(b)
+
+
+// var a=+prompt("enter the date")
+// var b=a
+// var c=b.getFullYear()
+// alert(c)
+
+
+
+// Specifying a date and time
+
+// let a= new Date("januray-4-2028");
+// console.log(a)
+
+
+// var a=prompt("enter date and time")
+// var b=new Date(a);
+// alert(b)
+
+
+// let a="2024-1-1 3:23:43";
+// let b=new Date(a.replace("","T")+ "Z");
+// // let c=b.getDate()
+// console.log("date object crated" + b)
+
+
+
+// Changing Elements of a date and time
+
+// var a=new Date
+// a.setDate(1)
+// console.log(a)
+
+
+// var a=new Date
+// a.setUTCHours(1)
+// console.log(a)
+
+
+
+
+
+
+
+
+
+
